58e8c3c28e03410495b53e3a5904f3d0b3df77a4
Givalter <givaltera@gmail.com>
2025-07-17 17:52:24 -0300

    inicio do js

---
+++ Tipos de dados/script.js
@@ -0,0 +1,16 @@
+//  7 tipos de variaveis
+var nome = 'Givalter'; // string
+    idade = 30; // number  // numero inteiro ou decimal
+    possuiFaculdade = true; // boolean  // true or false
+    time = 'Basil'; // undefined  // não foi definido
+    comida = 'null'; // null // não tem valor
+    simbolo = '?'; // Symbol()  // um tipo de dado que é único e não pode ser alterado
+    novoObjeto = {} // Object // um tipo de dado que é um objeto
+    
+console.log()
+      
+    
+
+
+
+
---
+++ variaveis/script.js
@@ -0,0 +1,32 @@
+// exercicio 1: declaração de variaveis
+
+
+// declarar variavel com seu nome
+var nome = 'Givalter';
+// declarar uma vareiavel com seu nome e idade
+var idade = 30;
+// declarar uma variavel com sua comida 
+// favorita e não atribuir valor  
+var comida;
+// atribuir valor a sua comida favorita
+comida = 'Bolo de maracuja';
+// declarar 5 variavris sem valor
+var peso, 
+    altura, 
+    imc, 
+    nome2, 
+    idade2;
+
+console.log(nome, idade, comida)
+
+
+
+
+
+
+      
+    
+
+
+
+
